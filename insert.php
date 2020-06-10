ecce6e37ff21f8b92b383d1b645d896b680001fc
yoimapren <yoimapren27@gmail.com>
2020-06-10 16:19:39 +0700

    after push bootstrap n style

--- insert.php
+++ insert.php
@@ -7,20 +7,11 @@
 <title>YoiMapren CRUD INSERT</title>
 <meta charset="UTF-8">
 <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<style>
-    body{font-family: 'Nunito', sans-serif;
-        font-weight: 200;
-        height: 100vh;
-        margin: auto;
-        margin-left: 12px;
-    }
-</style>
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="style/main.css">
+<script src="bootstrap/js/bootstrap.min.js"></script>
 </head>
-<body class="container">
+<body class="container" id="main">
     <h1>YoiMapren CRUD INSERT</h1>
     <h2>Insert Your Data Here!</h2><br>
     <form action="addproduct.php" method="post">
